fee30b73bfbdb2b60246456df93ebc9da24145cc
moadmmad773-boop <moadmmad773@gmail.com>
2026-08-24 02:12:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -64,5 +64,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`🚀 السيرفر يعمل بنجاح على البورت ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
